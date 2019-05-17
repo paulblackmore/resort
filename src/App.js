a6cd512be5adde8ad600d5c440cb06747df3f7f1
Paul Blackmore <paulmichaelblackmore@gmail.com>
2019-05-17 09:52:54 +0200

    imported BrowserRouer, Switch and Route and setup routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Rooms from './pages/Rooms/Rooms';
 import SingleRoom from './pages/SingleRoom/SingleRoom';
@@ -8,10 +9,11 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" component={Rooms} />
+        <Route path="/single-room" component={SingleRoom} />
+      </BrowserRouter>
     </React.Fragment>
   );
 }
