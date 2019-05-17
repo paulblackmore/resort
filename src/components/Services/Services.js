d70d4032a17510895422a91080ce909d2ac06f98
Paul Blackmore <paulmichaelblackmore@gmail.com>
2019-05-17 11:39:37 +0200

    services component setup with dynamic title component

---
+++ src/components/Services/Services.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Title from '../Title/Title';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
+
+export default class Services extends Component {
+	state = {
+		services: [
+			{
+				icon: <FaCocktail />,
+				title: "free cocktails",
+				info: "Lorem ipsum dolor amet etsy vaporware PBR&B, yr waistcoat celiac brunch asymmetrical coloring book blue bottle freegan. Microdosing drinking vinegar keytar."
+			},
+			{
+				icon: <FaHiking />,
+				title: "Hiking",
+				info: "Lorem ipsum dolor amet etsy vaporware PBR&B, yr waistcoat celiac brunch asymmetrical coloring book blue bottle freegan. Microdosing drinking vinegar keytar."
+			},
+			{
+				icon: <FaShuttleVan />,
+				title: "free shuttle",
+				info: "Lorem ipsum dolor amet etsy vaporware PBR&B, yr waistcoat celiac brunch asymmetrical coloring book blue bottle freegan. Microdosing drinking vinegar keytar."
+			},
+			{
+				icon: <FaBeer />,
+				title: "free beer",
+				info: "Lorem ipsum dolor amet etsy vaporware PBR&B, yr waistcoat celiac brunch asymmetrical coloring book blue bottle freegan. Microdosing drinking vinegar keytar."
+			}
+		]
+	}
+
+  render() {
+    return (
+      <section className="services">
+        <Title title="services"/>
+				<div className="services-center">
+		{this.state.services.map((item, index) => { 
+			return(
+				<article key={index} className="service">
+					<span>{item.icon}</span>
+					<h6>{item.title}</h6>
+					<p>{item.info}</p>
+				</article> 
+			)
+		})}
+				</div>
+      </section>
+    )
+  }
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../../components/Hero/Hero';
 import Banner from '../../components/Banner/Banner';
+import Services from '../../components/Services/Services';
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms from $200">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <React.Fragment>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms from $200">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </React.Fragment>
   )
 }
