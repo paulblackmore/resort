efc5c80f96a0e8113a25e5919ad1a5c3cbed6380
Paul Blackmore <paulmichaelblackmore@gmail.com>
2019-05-17 12:57:55 +0200

    conditionally rendering loading spinner or featured rooms based on state

--- src/components/FeaturedRooms/FeaturedRooms.js
+++ src/components/FeaturedRooms/FeaturedRooms.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { RoomContext } from '../../Context';
 import Loading from '../Loading/Loading';
 import Room from '../Room/Room';
+import Title from '../Title/Title';
 
 export default class FeaturedRooms extends Component {
 	static contextType = RoomContext;
   render() {
-    const { featuredRooms } = this.context;
-    console.log(featuredRooms);
+    let { loading, featuredRooms } = this.context;
+    let rooms = featuredRooms.map(room => {
+			return <Room key={room.id} room={room} />
+		})
     return (
-      <div>
-        <Loading />
-				<Room />
-      </div>
+      <section className="featured-rooms">
+				<Title title="featured rooms"/>
+				<div className="featured-rooms-center">
+					{loading ? <Loading/> : rooms}
+				</div>
+      </section>
     )
   }
 }
