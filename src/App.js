43541b9352807c10d7d455e347f9290ab30db43c
Paul Blackmore <paulmichaelblackmore@gmail.com>
2019-05-17 09:39:40 +0200

    component pages added and imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Home from './pages/Home/Home';
+import Rooms from './pages/Rooms/Rooms';
+import SingleRoom from './pages/SingleRoom/SingleRoom';
+import Error from './pages/Error/Error';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </React.Fragment>
   );
 }
 
