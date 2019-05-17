4f7196cc97b67cf1991cc9cf37c6231c4a1ed2f6
Paul Blackmore <paulmichaelblackmore@gmail.com>
2019-05-17 12:02:03 +0200

    context api setup

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Home from './pages/Home/Home';
 import Rooms from './pages/Rooms/Rooms';
 import SingleRoom from './pages/SingleRoom/SingleRoom';
 import Error from './pages/Error/Error';
+import { RoomProvider } from './Context';
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/" exact component={Rooms} />
-          <Route path="/rooms/:slug" exact component={SingleRoom} />
-          <Route component={Error} />
-        </Switch>
-      </BrowserRouter>
+      <RoomProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/" exact component={Rooms} />
+            <Route path="/rooms/:slug" exact component={SingleRoom} />
+            <Route component={Error} />
+          </Switch>
+        </BrowserRouter>
+      </RoomProvider>
     </React.Fragment>
   );
 }
